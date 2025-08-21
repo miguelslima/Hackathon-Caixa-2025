d0bb9b55f52a4beedc8e51b48458723174f01a2d
Miguel Lima <miguelsousalima@hotmail.com>
2025-08-20 22:28:21 -0300

    Ajustes na lib json-server e config server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const jsonServer = require('json-server');
-
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
-
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(process.env.PORT || 3001, () => {
-  console.log('JSON Server está rodando!');
+server.listen(process.env.PORT, () => {
+  console.log('JSON Server está rodando na Vercel!');
 });
+
+module.exports = server;
